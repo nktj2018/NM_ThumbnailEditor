4740e1ebed14bd7b21a0cdc8689229a399896755
nktj2018 <nktj_takakazu2014@yahoo.co.jp>
2019-03-16 12:19:13 +0900

    3/16

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require "vendor/autoload.php";
 $NTE=new nakatsuji\thumbnaileditor\ThumbnailEditor(array(
 	"language"=>"jp",
 	"outputWidth"=>300,
+	"retry"=>false,
 ));
 
 if(@$_POST){
@@ -16,6 +17,11 @@ if(@$_POST){
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+<title>Thumbnail Editor Sample</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
 <body>
 <style>
 a{
@@ -23,10 +29,17 @@ a{
 	cursor:pointer;
 
 }
-.buttons{
-	padding:6px 10px;
-	display:inline-block;
-	background:#e0e0e0;
+ul{
+	list-style-type:none;
+	overflow:hidden;
+	zoom:1;
+}
+label{
+	cursor:pointer;
+}
+ul li{
+	float:left;
+	margin-right:10px;
 }
 </style>
 <script src="jquery.js"></script>
@@ -34,22 +47,25 @@ a{
 	<h1>Thumbnail Editor</h1>
 
 	<form method="post">
+		<ul>
 		<?php
-		for($u1=0;$u1<3;$u1++){
+		for($u1=0;$u1<5;$u1++){
 		?>
-		<div style="margin-bottom:10px">
-			<label for="nte_ui" data-nte="nte000<?php echo $u1; ?>" class="buttons">click here</label>
-			<div style="margin-top:10px"><img src="" data-nte="nte000<?php echo $u1; ?>" style="width:200px"></div>
+		<li>
+			<label for="nte_ui" data-nte="nte000<?php echo $u1; ?>" class="buttons">
+				<div style="margin-top:10px"><img src="" data-nte="nte000<?php echo $u1; ?>" style="width:200px;height:200px;background:#c0c0c0;"></div>
+				<div>
+					<input type="hidden" name="<?php echo $u1; ?>[path]" data-nte="nte000<?php echo $u1; ?>" data-nte_mode="path">
+					<input type="hidden" name="<?php echo $u1; ?>[file_name]" data-nte="nte000<?php echo $u1; ?>" data-nte_mode="file_name">
+					<input type="hidden" name="<?php echo $u1; ?>[changed]" data-nte="nte000<?php echo $u1; ?>" data-nte_mode="changed">
+				</div>
+			</label>
 			<a data-nte="nte000<?php echo $u1; ?>" data-nte_mode="delete_btn">クリア</a>
-			<div>
-				<input type="hidden" name="<?php echo $u1; ?>[path]" data-nte="nte000<?php echo $u1; ?>" data-nte_mode="path">
-				<input type="hidden" name="<?php echo $u1; ?>[file_name]" data-nte="nte000<?php echo $u1; ?>" data-nte_mode="file_name">
-				<input type="hidden" name="<?php echo $u1; ?>[changed]" data-nte="nte000<?php echo $u1; ?>" data-nte_mode="changed">
-			</div>
-		</div>
+		</li>
 		<?php
 		}
 		?>
+		</ul>
 		<input type="submit" value="submit">
 	</form>
 </div>
